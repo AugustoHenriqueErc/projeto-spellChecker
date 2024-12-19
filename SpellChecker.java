0af872917ce24133f75ed4555dc51ab2dc990d6e
Augusto <aaugustohenrique05@gmail.com>
2024-12-19 07:25:06 -0300

    Adicionando classes insert, remove utilizando iterator e a classe isPalindrome

--- SpellChecker.java
+++ SpellChecker.java
@@ -6,10 +6,12 @@
  * @version (um número da versão ou uma data)
  */
 import java.util.ArrayList;
+import java.util.Iterator;
+
 public class SpellChecker
 {
     // variáveis de instância - substitua o exemplo abaixo pelo seu próprio
-    private ArrayList dicionário;
+    private ArrayList<String> dicionário;
     private int numberOfWords;
     /**
      * Construtor para objetos da classe SpellChecker
@@ -32,18 +34,83 @@ public class SpellChecker
         {
             return true;
         }
-            return false;
+        return false;
     }
     
     public boolean allKnownWords(ArrayList<String> words)
     {
         for(String word: words)
         {
-            if(dicionário.contains(word))
+            if(!dicionário.contains(word))
             {
-                
+                return false;
             }
         }
         return true;
     }
-}
+    
+    public ArrayList<String> wordsStartsWith(String prefix)
+    {
+        ArrayList<String> wordsStartsWith = new ArrayList<>();  
+        for(String word: dicionário)
+        {
+           if(word.startsWith(prefix))
+            {
+                wordsStartsWith.add(word);
+            }
+        }
+        return wordsStartsWith;
+    }
+    
+    public ArrayList<String> wordsContaining(String text)
+    {
+        ArrayList<String> wordsContaining = new ArrayList<>();  
+        for(String word: dicionário)
+        {
+           if(word.contains(text))
+            {
+                wordsContaining.add(word);
+            }
+        }
+        return wordsContaining;
+    }
+    
+    public void insert(String newWord)
+    {
+        if(!this.isKnownWord(newWord))
+        {
+            for(int i = 0; i < dicionário.size();i++)
+            {
+                 if(newWord.compareTo(dicionário.get(i)) > 0)
+                {
+                      dicionário.add(newWord);  
+                }
+            }
+        }
+    }
+    
+    public void remove(String removeWord)
+    {
+        Iterator<String> it = dicionário.iterator();
+        String search;
+        if(this.isKnownWord(removeWord))
+        {
+            while(it.hasNext())
+            {
+                search = it.next();
+                if(removeWord == search) it.remove();
+            }
+        }
+    }
+    
+    public boolean isPalindrome(String word)
+    {
+        if(this.isKnownWord(word))
+        {
+            word = word.toLowerCase();
+            String pali = new StringBuilder(word).reverse().toString();
+            if(word  == pali) return true;
+        }
+        return false;
+    }
+}   
